14a32ab1d14f068284c535223b00c9b576dcf2f3
ellisa <ellisaissa@gmail.com>
2021-07-07 23:35:48 -0400

    additions
    added to and moved folders around

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -8,7 +8,7 @@ function Nav() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark">
-                <Link className="nav-title" to="/">Google Books</Link>
+                <Link className="n-title" to="/">Google Books</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
